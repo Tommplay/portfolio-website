58f8df78f8852d03b4b53efd97560af1732c61cf
Tommplay <98472145+Tommplay@users.noreply.github.com>
2022-02-11 11:03:43 +0100

    Added scripts.js
    Added hamburger menu to navigation in Index, about, css, js files

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
+console.log("Welcome to my portfolio website");
 alert("Welcome to my portfolio website");
+console.log(thisVariableDoesntExist); // Will output undefined
+var emptyVariable = null;
+var notNullButAString = "null";
+console.log(doesntExist); // Outputs undefined
+console.log(emptyVariable); // Outputs Null
+console.log(notNullButAString); // Outputs "null" - not the same as null!
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
