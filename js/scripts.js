e87b60ed669c23670505e6977c4025c5970db456
Tommplay <98472145+Tommplay@users.noreply.github.com>
2022-02-22 14:24:53 +0100

    change index.html
    change soe text

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ function menuToggle() {
   var x = document.getElementById('myNavtoggle');
 
   if (x.className === 'navtoggle') {
-    
+
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
